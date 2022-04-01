82a99e6a44568a2327c80cb9726ad6cea3af6233
Piyush Chamoli <piyushchamoli11@gmail.com>
2022-04-01 18:52:30 +0530

    add

---
+++ block-BRaabm/code/movie-list-app/src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import data from '../data.json';
+import MovieDetails from './MovieDetails';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return 'hello'
+  }
+}
+
+export default App;
